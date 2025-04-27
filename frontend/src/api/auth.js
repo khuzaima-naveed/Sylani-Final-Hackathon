4f19051ece44a834d552230006ca28f3aeabedbf
khuzaima-naveed <khuzaimamirzaoct9@gmail.com>
2025-04-27 15:26:15 +0500

    First commit - project complete

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api/auth';
+
+export const signup = async (email, password) => {
+  try {
+    const response = await axios.post(`${API_URL}/signup`, { email, password });
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw error.response.data;
+    } else {
+      throw { message: 'Server error. Please try again later.' };
+    }
+  }
+};
+
+export const login = async (email, password) => {
+  try {
+    const response = await axios.post(`${API_URL}/login`, { email, password });
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw error.response.data;
+    } else {
+      throw { message: 'Server error. Please try again later.' };
+    }
+  }
+};
